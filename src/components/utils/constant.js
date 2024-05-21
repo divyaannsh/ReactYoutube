fd3a2674bf36aff3b2a3da4ec5f096b87b15d9b1
Divyansh <85442869+divyaannsh@users.noreply.github.com>
2024-05-21 22:35:33 +0530

    Watchpage

--- src/components/utils/constant.js
+++ src/components/utils/constant.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY="AIzaSyBwalB7Bxvhgl8alXV_vU5h3q1mIjxI14Q"
+// const GOOGLE_API_KEY="AIzaSyBwalB7Bxvhgl8alXV_vU5h3q1mIjxI14Q"
 
 export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyBwalB7Bxvhgl8alXV_vU5h3q1mIjxI14Q" 
 export const OFFSET_LIVE_CHAT=25
